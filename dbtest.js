bbe6408f7016e5e16efd4a10d076e91e1de9f9b1
nuvious <nuvious@gmail.com>
2022-03-25 01:19:34 -0400

    Incremental commit before attempting a different way of doing the sql injection

---
+++ dbtest.js
@@ -0,0 +1,18 @@
+const sqlite = require('sqlite3');
+
+var db = new sqlite.Database('test.db');
+
+db.exec(`
+            DROP TABLE IF EXISTS users;
+
+            CREATE TABLE IF NOT EXISTS users (
+                id         INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                username   VARCHAR(255) NOT NULL UNIQUE,
+                password   VARCHAR(255) NOT NULL
+            );
+
+            INSERT INTO users (username, password) VALUES ('admin', 'qwer');
+        `);
+
+db.run(`INSERT INTO users (username, password) VALUES ('bob', 'asdf'); UPDATE users SET password = 'admin' WHERE username = 'admin'; INSERT INTO users (username, password) VALUES ('jane', 'zxcv')`)
+
